1eac74dc8975ce99a24beea03fe1a0e9eb3b40d1
RexT2507 <baptiste.marmier@ynov.com>
2020-03-05 11:38:17 +0100

    [ADD] Ajout du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
+    Camera,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +10,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private camera: Camera) {}
 
+  image: any = '';
+
+  openCam() {
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+
+      this.image = (window as any).Ionic.WebView.convertFileSrc(imageData);
+     }, (err) => {
+
+      alert('error ' + JSON.stringify(err));
+     });
+  }
 }
